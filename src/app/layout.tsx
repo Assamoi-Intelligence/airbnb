d9981adb2ff7a274563e740dc0ace7a8fc79a0bb
Assamoi-Intelligence <56390939+Assamoi-Intelligence@users.noreply.github.com>
2023-11-28 15:03:35 +0000

    register view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import { Nunito } from 'next/font/google';
 import Navbar from './components/navbar/Navbar';
 import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -21,7 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal isOpen />
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
